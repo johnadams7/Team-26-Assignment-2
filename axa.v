dc263071c42ba5e3df539701e47aa1cd992d9f9a
David Tran <dv.tran99@gmail.com>
2019-10-05 15:46:21 -0400

    Added processor and testbench module

--- axa.v
+++ axa.v
@@ -10,7 +10,6 @@
 `define IMMED						[11:4]
 `define STATE						[5:0]
 `define REGS						[15:0]
-`define PC							r[15]
 `define OPERATION_BITS 	[5:0]
 `define REGSIZE					[15:0]
 
@@ -38,15 +37,84 @@
 `define OPlhi						6'b110000
 `define OPllo						6'b111000
 
+//State values
+`define Start						6'b111111
+`define Decode					6'b111110
+`define Done						6'b111101
+
 module ALU(out, in1, in2, op);
-parameter BITS 16;
+parameter BITS = 16;
 output [BITS-1:0] out;
 input `REGSIZE in1, in2;
 input `OPERATION_BITS op;
 reg `REGSIZE a;
-always @(in1, in2, op) #1
-	case op:
-		`OPadd : a <= in1 + in2;
-		`OPsub : a <= in1 - in2;
-		`OPxor : a <= in1 ^ in2;
-		'OProl : a <= {in1[in2:0], in1
+reg `REGSIZE temp;
+always @(in1 or in2 or op) begin #1
+	case(op)
+		`OPadd: begin a <= in1 + in2; end
+		`OPsub: begin a <= in1 - in2; end
+		`OPxor: begin a <= in1 ^ in2; end
+		`OProl: begin	a <= {in1 << in2, in1 >> (BITS - in2)}; end
+        endcase
+end
+
+endmodule
+
+module processor(halt, reset, clk);
+output reg halt;
+input reset, clk;
+
+reg `DATA reglist `REGSIZE;
+reg `DATA datamem `SIZE;
+reg `INSTRUCTION instrmem `SIZE;
+reg `DATA pc = 0;
+reg `INSTRUCTION ir;
+reg `STATE s;
+reg `DATA passreg;
+
+always @(reset) begin
+	halt <= 0;
+	pc <= 0;
+	s <= `Start;
+//Setting initial values
+	$readmemh0(reglist); //Registers
+	$readmemh1(datamem); //Data
+	$readmemh2(instrmem); //Instructions
+end
+
+always @(posedge clk) begin
+	case(s)
+		`Start: begin
+			ir <= instrmem[pc];
+			s <= `Decode;
+			end
+		`Decode: begin
+			pc <= pc + 1;
+			s <= `Done;
+                        end
+		default: begin
+			halt <= 1;
+			end
+		endcase
+end
+
+endmodule
+
+module testbench;
+reg reset = 0;
+reg clk = 0;
+wire halt;
+
+processor PE(halt, reset, clk);
+initial begin
+	$dumpfile;
+	$dumpvars(0, PE);
+	#10 reset = 1;
+	#10 reset = 0;
+	while (!halt) begin
+		#10 clk = 1;
+		#10 clk = 0;
+	end
+	$finish;
+end
+endmodule
